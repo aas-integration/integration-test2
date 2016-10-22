8838e7ad2f3d93477a8fe2b3ff6c4782f50513e7
langkymus <li@pascali>
2016-10-21 21:55:24 -0700

    better plots

--- plot_hist.py
+++ plot_hist.py
@@ -72,23 +72,24 @@ def show_improvement(proj, dot_score_lst_nc, dot_score_lst_c, dot_sim_res_nc, do
 	nc_total = 0.0
 	c_total = 0.0
 	largest_impr = 0.0
+        method_num = len(dot_score_lst_nc)
 	impr_lst = []
 	assert len(dot_score_lst_nc)==len(dot_score_lst_c), "Should have the same number of methods with or without clustering."
 	for i in range(len(dot_score_lst_nc)):
 		assert dot_score_lst_nc[i][0]==dot_score_lst_c[i][0], "Should be comparing the same dot."
 		nc_total += dot_score_lst_nc[i][1]
 		c_total += dot_score_lst_c[i][1]
-		impr = dot_score_lst_c[i][1] - ot_score_lst_nc[i][1]
+		impr = dot_score_lst_c[i][1] - dot_score_lst_nc[i][1]
 		if impr > largest_impr:
 			largest_impr = impr
 		#assert impr_score+0.00001>=0.0, "Clustering should not degrade the performance of similar program identification."
-		impr_lst.append((dot_score_lst_nc[i][0], impr_score))
+		impr_lst.append((dot_score_lst_nc[i][0], impr))
         impr_lst.sort(key=lambda x: x[1], reverse=True)
-    total_impr = c_total - nc_total
+        total_impr = c_total - nc_total
 	print("\n***************************\n")
 	print("{0}:\n".format(proj))        
-	print("Average similarity score improvement per method: {0}.\n".format(total_impr/len(dot_score_lst_nc)))
-	print("Percentage score improvement: {0}.\n".format(total_impr/nc_total))
+	print("Average score improvement per method: {0}.\n".format(total_impr/len(dot_score_lst_nc)))
+	print("Percentage score improvement: {0}.\n".format(total_impr*100/nc_total))
 	print("Largest score improvement for a single method: {0}.\n".format(largest_impr))
 	print("The top {0} most improved methods are:\n".format(topk))
 	for i in range(topk):
@@ -105,17 +106,20 @@ def show_improvement(proj, dot_score_lst_nc, dot_score_lst_c, dot_sim_res_nc, do
                 print("\n")
 	print("\n***************************\n")
 	# output some stats
-	return (avg_impr, percent_impr, largest_impr)
+	return (c_total, nc_total, method_num, largest_impr)
 
-def plot_hist(x, xlabel, y, ylabel, fig_file, avg_impr, percent_impr, largest_impr):
-	perf_labels = ["Avg score improvement: ".format(avg_impr), "\% score improvement: ".format(percent_impr*100), "Largest single improvement: ".format(largest_impr)]
-	bins = numpy.linspace(0.0, 2.0, 100)
+def plot_hist(x, xlabel, y, ylabel, fig_file, title=""):
+	bins = numpy.linspace(0.0, 4.0, 100)
 	#pyplot.hist(x, bins, alpha=0.5, label=xlabel)
 	#pyplot.hist(y, bins, alpha=0.5, label=ylabel)
 	data = numpy.vstack([x, y]).T
-	pyplot.hist(data, bins, alpha=0.7, label=[xlabel, ylabel]+perf_labels)
-	pyplot.legend(loc="upper right")
+        pyplot.figure()
+	pyplot.hist(data, bins, alpha=0.7, color=["white", "black"], hatch="//", label=[xlabel, ylabel])
+	pyplot.legend(loc="upper right")        
 	#pyplot.show()
+        pyplot.title(title)
+        pyplot.ylabel("number of program segments")
+        pyplot.xlabel("similarity score")
 	pyplot.xlim(0.0, 1.0)
 	pp = PdfPages(fig_file+".pdf")
 	pyplot.savefig(pp, format='pdf')
@@ -142,14 +146,18 @@ def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-nc", "--nocluster", required=True, type=str, help="path to the result folder without relabeling")
 	parser.add_argument("-c", "--cluster", required=True, type=str, help="path to the result folder with relabeling")
-	parser.add_argument("-f", "--fig", required=True, type=str, help="path to the figure folder")
-	parser.add_argument("-s", "-strategy", required=True, type=str, help="name of the strategy")
+	parser.add_argument("-f", "--fig", type=str, help="path to the figure folder")
+	parser.add_argument("-s", "--strategy", required=True, type=str, help="name of the strategy")
 	parser.add_argument("-k", "--topk", type=int, help="top k most improved methods")
-	parser.add_argument("-a", "--all", action="store_true", help="set to merge results from all benchmark projects in a single histogram")
+	#parser.add_argument("-a", "--all", action="store_true", help="set to merge results from all benchmark projects in a single histogram")
 	args = parser.parse_args()
 
 	proj_lst = common.LIMITED_PROJECT_LIST
-	common.mkdir(args.fig)
+
+        fig_dir = args.strategy+"_hist"
+        if args.fig:
+            fig_dir = args.fig
+	common.mkdir(fig_dir)
 
 	dot_method_map = get_dot_method_map(proj_lst)
 
@@ -160,21 +168,35 @@ def main():
 	all_score_lst_nc = []
 	all_score_lst_c = []
 
+        all_c_total = 0.0
+        all_nc_total = 0.0
+        all_largest_impr = 0.0
+        all_method_num = 0
 	for proj in proj_lst:
 	    proj_result_file_name = proj + "_result.txt"
 	    (dot_lst_nc, dot_res_nc) = parse_result_file(os.path.join(args.nocluster, proj_result_file_name))
 	    (dot_lst_c, dot_res_c) = parse_result_file(os.path.join(args.cluster, proj_result_file_name))
 	    score_lst_nc = [x[1] for x in dot_lst_nc]
 	    score_lst_c = [x[1] for x in dot_lst_c]
-	    if args.all:
-	    	all_score_lst_nc += score_lst_nc
-	    	all_score_lst_c += score_lst_c
-	    else:
-			show_improvement(proj, dot_lst_nc, dot_lst_c, dot_res_nc, dot_res_c, dot_method_map, topk)
-			plot_hist(score_lst_nc, "w/o clustering", score_lst_c, args.strategy, os.path.join(args.fig, proj))
-			print("\n")
-	if args.all:
-		plot_hist(all_score_lst_nc, "w/o clustering", all_score_lst_c, args.strategy, os.path.join(args.fig, "all"))		
+            
+            (c_total, nc_total, method_num, largest_impr) = show_improvement(proj, dot_lst_nc, dot_lst_c, dot_res_nc, dot_res_c, dot_method_map, topk)
+            all_c_total += c_total
+            all_nc_total += nc_total
+            all_method_num += method_num
+            if largest_impr > all_largest_impr:
+                all_largest_impr = largest_impr
+            all_score_lst_nc += score_lst_nc
+            all_score_lst_c += score_lst_c
+	    
+            plot_hist(score_lst_nc, "w/o clustering", score_lst_c, args.strategy, os.path.join(fig_dir, proj), proj+" : "+args.strategy)
+            print("\n")
+	
+        all_avg_impr = (all_c_total - all_nc_total)/all_method_num
+        all_percent_impr = (all_c_total - all_nc_total)*100/all_nc_total
+        plot_hist(all_score_lst_nc, "w/o clustering", all_score_lst_c, args.strategy, os.path.join(fig_dir, args.strategy), "all : "+args.strategy)
+        print("Average score improvement across projects: {0}".format(all_avg_impr))
+        print("Percentage score improvement across projects: {0}".format(all_percent_impr))
+        print("Largest score improvement for a single method: {0}".format(all_largest_impr))
 
 if __name__ == "__main__":
     main()
