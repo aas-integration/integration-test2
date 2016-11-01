86b2d64c3fb9ddb33f54099a48c838326cf9f7c7
schaef <martinschaef@gmail.com>
2016-11-01 09:45:38 -0400

    updated prog2dfg dependency and added heatmap script

--- fetch_dependencies.sh
+++ fetch_dependencies.sh
@@ -10,7 +10,7 @@ pushd libs &> /dev/null
 JARS=(
     "http://www.csl.sri.com/users/schaef/jars/daikon.jar"
     "https://github.com/randoop/randoop/releases/download/v3.0.3/randoop-all-3.0.3.jar"
-    "https://www.dropbox.com/s/i1iqgf9w7jk1x3x/prog2dfg.jar"
+    "https://github.com/aas-integration/prog2dfg/releases/download/v0.1/prog2dfg.jar"
     "https://github.com/junit-team/junit/releases/download/r4.12/junit-4.12.jar"
     "http://search.maven.org/remotecontent?filepath=org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar"
     "https://github.com/petablox-project/petablox/releases/download/v1.0/petablox.zip"
---
+++ make_heatmaps.py
@@ -0,0 +1,67 @@
+import sys, os, shutil
+from PIL import Image
+
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import numpy as np
+
+def main(file_name):
+  print ("Opening {}".format(file_name))
+
+  w,h = 10, 10
+  matrix = [[0 for x in range(w)] for y in range(h)]
+
+  with open(file_name, 'r') as f:
+    content = f.readlines()
+    if len(content)!=2:
+      print ("Bad file format")
+      return
+    semantic_sim = [i.strip() for i in content[0].split(',')]
+    syntactic_sim = [i.strip() for i in content[1].split(',')]
+
+    print ("Number of points: {}".format(len(semantic_sim)))
+
+    if len(semantic_sim)!=len(syntactic_sim):
+      print ("Bad file format")
+      return
+
+    max_c = 0
+    for i in range(len(semantic_sim)):
+      x = int(float(semantic_sim[i])*float(w-1))
+      y = int(float(syntactic_sim[i])*float(h-1))
+      matrix[x][y] += 1
+      max_c = max(max_c, matrix[x][y])
+
+
+    # sample to a matrix of size 100x100
+    
+    imgdata = [[0 for x in range(100)] for y in range(100)]
+    for x in range(100):
+      for y in range(100):
+        imgdata[x][y] = matrix[int(float(x)/100.0 * w)][int(float(y)/100.0 * h)]
+
+
+
+    plt.imshow(imgdata, cmap='hot', interpolation='nearest')
+    plt.colorbar() 
+    plt.gca().invert_yaxis()
+
+    plt.ylabel('name similarity')
+    plt.xlabel('semantic similarity')
+
+    plt.savefig(file_name.replace('.txt', '.pdf'))
+
+    #plt.show()
+
+    # img = Image.new( 'RGB', (w,h), "white") # create a new black image
+    # pixels = img.load() # create the pixel map
+
+    # for x in range(w):
+    #   for y in range(h):
+    #     c = 255 - int(float(matrix[x][y])*recolor_factor)
+    #     pixels[x,y] = (c,c,c)
+
+    # img.show()
+    
+if __name__ == '__main__':
+  main(sys.argv[1])
