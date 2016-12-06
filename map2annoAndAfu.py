907b4bc39371d3fb60358f0334ed709823983221
CharlesZ-Chen <charleszhuochen@gmail.com>
2016-12-05 23:44:48 -0500

    change to generate a single jaif file for the corpus. more documentation. slight change on jaif head value (add full qualified name of the enum field of Ontology annotation declaration.)

--- map2annoAndAfu.py
+++ map2annoAndAfu.py
@@ -10,41 +10,43 @@ MAP_WORKING_DIR = os.path.dirname(os.path.realpath(__file__))
 JAIF_FILE_ONTOLOGY_HEADER ="\
 package ontology.qual:\n\
   annotation @Ontology:\n\
-    enum OntologyValue[] values\n"
+    enum ontology.qual.OntologyValue[] values\n"
 
-def main():
-    json_file = "<TBD>"
-    
+def main(json_file):
     with open(json_file) as data_file:
         data = json.load(data_file)
 
     mappings = data["mappings"]
     ontology_set = set()
+    project_list = common.get_project_list()
 
     for mapping in mappings:
         ontology_set.add(mapping['label'][0])
 
-    # convertToOntologyValue(ontology_set)
+    convertToOntologyValue(ontology_set)
 
-    # this is easy to extend to multiple projects
-    Catalano_mappings = list()
-    for mapping in mappings:
-        # assume 'fields' at least has one element
-        if (mapping['fields'][0].startswith('Catalano')):
-            Catalano_mappings.append(mapping)
+    corpus_jaif_file = createSingleJairFile4Corpus(mappings)
 
-    createProjectJaifFile('catalano', Catalano_mappings)
-    runInsertAnnoToProject('catalano')
-    # frontend_pa_inference.run_inference('catalano')
+    for project in project_list:
+        runInsertAnnoToProject(project, corpus_jaif_file)
 
+    for project in project_list:
+        frontend_pa_inference.run_inference(project)
 
+## comment out this method if choosing using per project based jaif file to insert annotation
+# def runInsertAnnoToProject(project):
+#     """ Insert annotation info in default jaif file to ${project}  .
+#         default jaif file location would under ${project_dir} and named "${project}.jaif"
+#     """
+#     project_dir = common.get_project_dir(project)
+#     default_jaif_file = os.path.join(project_dir, "{}.jaif".format(project))
+#     runInsertAnnoToProject(project, default_jaif_file)
 
-def runInsertAnnoToProject(project):
+def runInsertAnnoToProject(project, jaif_file):
+    """ Insert annotation info in the ${jaif_file} to ${project}.
+    """
     project_dir = common.get_project_dir(project)
-    jaif_file = os.path.join(project_dir, "{}.jaif".format(project))
     with common.cd(project_dir):
-        # clean project first for testing purpose, should think about whether should clean at this point
-        # common.clean_project(project)
         common.setup_checker_framework_env()
         insert_cmd = ['insert-annotations-to-source', '-i', jaif_file]
         # using glob2.glob to recursive get java files under project dir
@@ -52,11 +54,19 @@ def runInsertAnnoToProject(project):
         insert_cmd.extend(java_files)
         common.run_cmd(insert_cmd, print_output=True)
 
-def createProjectJaifFile(project, mappings):
+def createSingleJairFile4Corpus(mappings):
+    return createJaifFile("corpus", mappings)
+
+def createJaifFile(project, mappings):
     """ create a {project_name}.jaif file under project_dir
         this jair file contains the insertted annotations info for this project
+        Note: if ${project} value is "corpus", then it will create a "corpus.jaif" under corpus dir
     """
-    project_dir = common.get_project_dir(project)
+    if project == "corpus":
+        project_dir = common.CORPUS_DIR
+    else:
+        project_dir = common.get_project_dir(project)
+
     jaif_file = os.path.join(project_dir, "{}.jaif".format(project))
 
     print ("Writing project {} annotated info to file {}".format(project, jaif_file))
@@ -85,8 +95,13 @@ def createProjectJaifFile(project, mappings):
                 for field, value_set in fields.items():
                     out_file.write("    field {}:\n".format(field))
                     out_file.write("    @Ontology(values={{{value_name}}})\n".format(value_name=', '.join(value_set)))
+    return jaif_file
 
 def parseField(field):
+    """ given a ${field} which describes a full qualified path to a class field with value like
+        "xxx.xxx.xxx.Class.field", parse it to a tuple of package path, class name, and field name as
+        (xxx.xxx.xxx, Class, field)
+    """
     return tuple(field.rsplit('.', 2))
 
 def convertToOntologyValue(ontology_set):
@@ -100,4 +115,5 @@ def convertToOntologyValue(ontology_set):
     pa2checker.recompile_checker_framework()
 
 if __name__ == '__main__':
-  main()
+    json_file = "<path-to>/test.json"
+    main()
