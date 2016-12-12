a7a612447e14b0fc2f27f6758bb17dd60fea91d1
Langkymus <liwenchaoapollo@gmail.com>
2016-12-12 00:22:37 -0500

    minor improvement

--- run_simprog.py
+++ run_simprog.py
@@ -17,11 +17,6 @@ def generate_graphs(project):
                   ['--graph-jar', common.get_jar('prog2dfg.jar'),
                    '--cache'])
 
-def generate_dtrace(project):
-  #TODO: set the out file to common.get_dtrace_file_for_project(project)
-  common.run_dljc(project,
-                  ['dyntrace'], ['--cache'])  
-
 def gather_kernels(projects, corpus_kernel_file):
   print("Gathering kernels from projects {0}".format(",".join(projects)))
   with open(corpus_kernel_file, "w") as corpus_kernel_file_handle:
@@ -110,6 +105,7 @@ def check_similarity(project, result_file, kernel_file, corpus_dot_to_method_map
   	for dot_file in this_method_map.keys():
   		dot_method = corpus_dot_to_method_map[dot_file]
   		json_result[dot_method] = []
+      print("Computing similar programs for {0}.".format(dot_method))
   		result_program_list_with_score = sim.find_top_k_similar_graphs(dot_file, dot_file, top_k, iter_num, cluster_json)
   		line = dot_file+":\n"
   		for (dt, score) in result_program_list_with_score:
